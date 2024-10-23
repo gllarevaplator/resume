4e52747fea199f5649cb35447837447e61cf5926
gllarevaplator <gllarevap@gmail.com>
2024-10-23 18:15:14 +0200

    Remove classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className=""></main>
-      <footer className=""></footer>
+    <div>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
